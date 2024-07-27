49ab70a9141652a6428ca7fe5ec65f3964b71aae
Arash Rasoulian <arashrasoulian1@gmail.com>
2024-07-27 20:58:20 +0200

    add redux to project

--- frontend/src/components/Logout.js
+++ frontend/src/components/Logout.js
@@ -1,29 +1,41 @@
-const Logout =({setCurrUser})=>{
-  const logout=async (setCurrUser)=>{
-      try {
-          const response=await fetch("http://localhost:3000/logout",{
-              method: "delete",
-              headers: {
-                  "content-type": "application/json",
-                  "authorization": localStorage.getItem("token")
-              },
-          })
-          const data=await response.json()
-          if(!response.ok) throw data.error
-          localStorage.removeItem("token")
-          setCurrUser(null)
-      } catch (error) {
-          console.log("error", error)
-      }
-  }
-  const handleClick=e=>{
-      e.preventDefault()
-       logout(setCurrUser)
-  }
+// src/components/Logout.js
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { clearCurrUser } from '../store/userSlice';
+
+const Logout = () => {
+  const dispatch = useDispatch();
+
+  const logout = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/logout", {
+        method: "delete",
+        headers: {
+          "content-type": "application/json",
+          "authorization": localStorage.getItem("token")
+        },
+      });
+
+      const data = await response.json();
+      if (!response.ok) throw data.error;
+
+      localStorage.removeItem("token");
+      dispatch(clearCurrUser());
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    logout();
+  };
+
   return (
-      <div>
-          <input type="button" value='Logout' onClick={handleClick}/>
-      </div>
-  )
-}
-export default Logout
+    <div>
+      <input type="button" value='Logout' onClick={handleClick} />
+    </div>
+  );
+};
+
+export default Logout;

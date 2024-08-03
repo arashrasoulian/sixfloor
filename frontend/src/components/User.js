1c418e5bea8d81afd2151d6548919010342198e0
Arash Rasoulian <arashrasoulian1@gmail.com>
2024-08-03 20:26:47 +0200

    complete profile

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,39 +1,41 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-  Link,
+  
 } from "react-router-dom";
+import { setCurrUser } from "../store/userSlice"; // Action to set current user in Redux
 import { Navbar } from "./navbar/Navbar";
 import { Profile } from "../pages/profile/Profile";
-
 import Homepage from "../pages/homepage/Homepage";
 import Landingpage from "../pages/landingpage/Landingpage";
 import Signin from "../pages/signinsignup/Signin";
 import Signup from "../pages/signinsignup/Signup";
 
-const ProtectedRoute = ({ children ,currUser}) => {
-  console.log("pro",currUser);
-  // const { isAuthenticated } = useAuth();
+const ProtectedRoute = ({ children }) => {
+  const currUser = useSelector((state) => state.user.currUser);
   return currUser ? children : <Navigate to="/landing" replace />;
 };
 
-// Public Route component (accessible only when not authenticated)
-const PublicRoute = ({ children ,currUser}) => {
-  console.log("pub",currUser);
-
-  // const { isAuthenticated } = useAuth();
+const PublicRoute = ({ children }) => {
+  const currUser = useSelector((state) => state.user.currUser);
   return !currUser ? children : <Navigate to="/home" replace />;
 };
 
-
-
 const User = () => {
   const currUser = useSelector((state) => state.user.currUser);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    // Check localStorage for user data on app load
+    const storedUser = localStorage.getItem("currUser");
+    if (storedUser) {
+      dispatch(setCurrUser(JSON.parse(storedUser))); // Hydrate the state with stored user data
+    }
+  }, [dispatch]);
 
   return (
     <Router>
@@ -44,16 +46,16 @@ const User = () => {
             path="/"
             element={
               currUser ? (
-                <Link to="/home"  replace/>
+                <Navigate to="/home" replace />
               ) : (
-                <Link to="/landing" replace />
+                <Navigate to="/landing" replace />
               )
             }
           />
           <Route
             path="/landing"
             element={
-              <PublicRoute currUser={currUser}>
+              <PublicRoute>
                 <Landingpage />
               </PublicRoute>
             }
@@ -61,7 +63,7 @@ const User = () => {
           <Route
             path="/signin"
             element={
-              <PublicRoute currUser={currUser}>
+              <PublicRoute>
                 <Signin />
               </PublicRoute>
             }
@@ -77,7 +79,7 @@ const User = () => {
           <Route
             path="/home"
             element={
-              <ProtectedRoute currUser={currUser}>
+              <ProtectedRoute>
                 <Homepage />
               </ProtectedRoute>
             }
@@ -85,7 +87,7 @@ const User = () => {
           <Route
             path="/profile"
             element={
-              <ProtectedRoute currUser={currUser}>
+              <ProtectedRoute>
                 <Profile />
               </ProtectedRoute>
             }
